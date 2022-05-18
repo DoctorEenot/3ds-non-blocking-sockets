24bb1b9749d3e9e512d328350745a3ffdfe65aa9
DoctorEenot <dreenot@gmail.com>
2022-05-18 03:25:36 +0300

    improvements

--- ODIN_socket/source/ODIN_socket.c
+++ ODIN_socket/source/ODIN_socket.c
@@ -19,52 +19,45 @@
 #define SOC_ALIGN       0x1000
 #define SOC_BUFFERSIZE  0x100000
 
+#define PIXELS_AMOUNT 96000
+#define BYTES_PER_BATCH 512
+#define PIXELS_PER_BATCH 256
+#define TOP_WIDTH 400
+#define TOP_HEIGHT 240
+
 static u32* SOC_buffer = NULL;
-s32 sock = -1, csock = -1;
+
+PrintConsole topScreen, bottomScreen;
 
 __attribute__((format(printf, 1, 2)))
 void failExit(const char* fmt, ...);
 
-
-//---------------------------------------------------------------------------------
-void socShutdown() {
-	//---------------------------------------------------------------------------------
-	printf("waiting for socExit...\n");
-	socExit();
-
+void print_bottom(char* text){
+	consoleSelect(&bottomScreen);
+	printf(text);
+}
+void print_top(char* text){
+	consoleSelect(&topScreen);
+	printf(text);
 }
 
-//---------------------------------------------------------------------------------
-int main(int argc, char** argv) {
-	//---------------------------------------------------------------------------------
-	int ret;
-	bool stop = true;
+void init(){
+	// Init main parameters
 
-	u32	clientlen;
-	struct sockaddr_in client;
-	struct sockaddr_in server;
-	char temp[1024];
-
+	print_bottom("\nPerforming init\n");
 	gfxInitDefault();
-
-	// register gfxExit to be run when app quits
-	// this can help simplify error handling
+	
 	atexit(gfxExit);
 
-	PrintConsole topScreen, bottomScreen;
 	consoleInit(GFX_TOP, &topScreen);
 	consoleInit(GFX_BOTTOM, &bottomScreen);
-	consoleSelect(&topScreen);
-
-	printf("\nlibctru sockets demo\n");
 
 	// allocate buffer for SOC service
 	SOC_buffer = (u32*)memalign(SOC_ALIGN, SOC_BUFFERSIZE);
-
 	if (SOC_buffer == NULL) {
 		failExit("memalign: failed to allocate\n");
 	}
-
+	
 	// Now intialise soc:u service
 	if ((ret = socInit(SOC_buffer, SOC_BUFFERSIZE)) != 0) {
 		failExit("socInit: 0x%08X\n", (unsigned int)ret);
@@ -74,42 +67,115 @@ int main(int argc, char** argv) {
 	// atexit functions execute in reverse order so this runs before gfxExit
 	atexit(socShutdown);
 
-	// libctru provides BSD sockets so most code from here is standard
-	clientlen = sizeof(client);
+	print_bottom("\nInit complet\n");
+}
 
-	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 
-	if (sock < 0) {
-		failExit("socket: %d %s\n", errno, strerror(errno));
+unsigned long long pixel_position = 0;
+void print_buffer(u8* frame_buffer, u16* pixels){
+	// writes pixels to the screen
+	// the amount of pixels specified in PIXELS_PER_BATCH
+
+	if(pixel_position == PIXELS_AMOUNT){
+		pixel_position = 0;
 	}
 
+	// end of the array
+	u32 pixels_end = pixels + BYTES_PER_BATCH;
+
+	// iterate over pixels
+	for(pixels; pixels<pixels_end; pixels += 2){
+		u16 pixel = *pixels;
+
+		uint8_t r = (pixel >> 11) << 3;
+		uint8_t g = ((pixel >> 5) & 0x3F) << 2;
+		uint8_t b = (pixel & 0x1F) << 3;
+
+		frame_buffer[pixel_position] = r;
+		frame_buffer[pixel_position+1] = g;
+		frame_buffer[pixel_position+2] = b;
+
+		pixel_position += 1;
+
+	}
+
+}
+
+
+void socShutdown() {
+	print_bottom("waiting for socExit...\n");
+	socExit();
+
+}
+
+
+int main(int argc, char** argv) {
+	int ret;
+
+	bool run_main_loop = true;
+
+	struct sockaddr_in client;
+	struct sockaddr_in server;
+	
+	u8 recv_buffer[BYTES_PER_BATCH];
+	u32 last_recieved_size = 0;
+
+	u32	clientlen = sizeof(client);
+	s32 server_sock = -1, client_sock = -1;
+
+	u8* frame_buffer = gfxGetFramebuffer(GFX_TOP,
+										 GFX_LEFT,
+										 NULL, 
+										 NULL);
+
+	// main init
+	memset(frame_buffer, 0, 240*400*3);
+	memset(recv_buffer, 0, BYTES_PER_BATCH);
 	memset(&server, 0, sizeof(server));
 	memset(&client, 0, sizeof(client));
+	init();
 
 	server.sin_family = AF_INET;
 	server.sin_port = htons(80);
 	server.sin_addr.s_addr = gethostid();
 
-	printf("Connect to %s\n", inet_ntoa(server.sin_addr));
+	// create server socket
+	server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+	if (server_sock < 0) {
+		failExit("socket: %d %s\n", errno, strerror(errno));
+	}
 
-	if ((ret = bind(sock, (struct sockaddr*)&server, sizeof(server)))) {
-		close(sock);
+	print_bottom("Connect to %s\n", inet_ntoa(server.sin_addr));
+
+	// bind server socket
+	if ((ret = bind(server_sock, (struct sockaddr*)&server, sizeof(server)))) {
+		close(server_sock);
 		failExit("bind: %d %s\n", errno, strerror(errno));
 	}
 
-	// Set socket non blocking so we can still read input to exit
-	fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
+	// Set  server socket non blocking 
+	fcntl(server_sock, F_SETFL, fcntl(server_sock, F_GETFL, 0) | O_NONBLOCK);
 
-	if ((ret = listen(sock, 5))) {
+	if ((ret = listen(server_sock, 1))) {
 		failExit("listen: %d %s\n", errno, strerror(errno));
 	}
 
-	while (aptMainLoop()) {
+	// start main app loop
+	while (aptMainLoop() & run_main_loop) {
 		hidScanInput();
-		u32 kDown = hidKeysDown();
-		csock = accept(sock, (struct sockaddr*)&client, &clientlen);
-		if (kDown & KEY_START) break;
-		if (csock < 0) {
+
+		// read pressed key
+		u32 pressed_key = hidKeysDown();
+
+		// accept connection
+		client_sock = accept(server_sock, (struct sockaddr*)&client, &clientlen);
+		
+		
+		if (pressed_key & KEY_START){
+			break;
+		}
+
+		if (client_sock < 0) {
 			if (errno != EAGAIN) {
 				failExit("accept: %d %s\n", errno, strerror(errno));
 			}
@@ -117,64 +183,74 @@ int main(int argc, char** argv) {
 		else {
 			// set client socket to blocking to simplify sending data back
 			//fcntl(csock, F_SETFL, fcntl(csock, F_GETFL, 0) & ~O_NONBLOCK);
-			printf("Connecting port %d from %s\n", client.sin_port, inet_ntoa(client.sin_addr));
-			while (csock != -1) {
+			print_bottom("Connecting port %d from %s\n", client.sin_port, inet_ntoa(client.sin_addr));
+			
+			while (client_sock != -1) {
+				
+				// recieving image
+				u32 result = recv(client_sock, 
+									recv_buffer + last_recieved_size,
+									BYTES_PER_BATCH-last_recieved_size, 0);
+				last_recieved_size += result;
+				if (last_recieved_size == BYTES_PER_BATCH) {
+					// recieved data
+					last_recieved_size = 0;
+
+					print_buffer(frame_buffer,recv_buffer)
+
+					memset(recv_buffer, 0, BYTES_PER_BATCH);
+				}else if(result == -1){
+					// socket error
+				}
+
+				// scan and process pressed key
 				hidScanInput();
-				circlePosition cpos;
-				hidCircleRead(&cpos);
-				consoleSelect(&topScreen);
-				u32 kDown = hidKeysDown();
-				memset(temp, 0, 1024);
-				recv(csock, temp, 1024, 0);
-				if (strcmp(temp, "") != 0) {
-					consoleSelect(&bottomScreen);
 
-					printf("%s\n", temp);
-					consoleSelect(&topScreen);
-				}
-				if (kDown & KEY_START) break;
-				if (kDown & KEY_A) {
-					send(csock, "A,", 2, 0);
-					printf("You pressed A\n");
-				}
-				if (kDown & KEY_B) {
-					send(csock, "B,", 2, 0);
-					printf("You pressed B\n");
-				}
-				if (kDown & KEY_X) {
-					send(csock, "X,", 2, 0);
-					printf("You pressed X\n");
-				}
-				if (kDown & KEY_Y) {
-					send(csock, "Y,", 2, 0);
-					printf("You pressed Y\n");
-				}
-				if (kDown & KEY_SELECT) {
-					send(csock, "SELECT,", 7, 0);
-					printf("You pressed SELECT\n");
-				}
-				if (kDown & KEY_DUP) {
-					send(csock, "u,", 2, 0);
-					printf("You pressed dUP\n");
-				}
-				if (kDown & KEY_DDOWN) {
-					send(csock, "d,", 2, 0);
-					printf("You pressed dDOWN\n");
+				// read circle position
+				circlePosition circle_position;
+				hidCircleRead(&circle_position);
+
+				// read pressed key
+				u32 pressed_key = hidKeysDown();
+
+				if (pressed_key & KEY_START){
+					run_main_loop = false; 
+					break;
+				}else if (pressed_key & KEY_A) {
+					send(client_sock, "A,", 2, 0);
+					print_bottom("You pressed A\n");
+				}else if (pressed_key & KEY_B) {
+					send(client_sock, "B,", 2, 0);
+					print_bottom("You pressed B\n");
+				}else if (pressed_key & KEY_X) {
+					send(client_sock, "X,", 2, 0);
+					print_bottom("You pressed X\n");
+				}else if (pressed_key & KEY_Y) {
+					send(client_sock, "Y,", 2, 0);
+					print_bottom("You pressed Y\n");
+				}else if (pressed_key & KEY_SELECT) {
+					send(client_sock, "SELECT,", 7, 0);
+					print_bottom("You pressed SELECT\n");
+				}else if (pressed_key & KEY_DUP) {
+					send(client_sock, "u,", 2, 0);
+					print_bottom("You pressed dUP\n");
+				}else if (pressed_key & KEY_DDOWN) {
+					send(client_sock, "d,", 2, 0);
+					print_bottom("You pressed dDOWN\n");
 				}
 
-				if (((cpos.dy > 25) || (cpos.dy < -25)) || ((cpos.dx > 25) || (cpos.dx < -25))) {
-					stop = false;
+				if (((circle_position.dy > 25) || (circle_position.dy < -25)) || ((circle_position.dx > 25) || (circle_position.dx < -25))) {
 					char posit[20];
-					sprintf(posit, "%d;%d,", cpos.dx, cpos.dy);
-					send(csock, posit, strlen(posit), 0);
-					printf("Circle x y pos is %d :  %d\n", cpos.dx, cpos.dy);
+					sprintf(posit, "%d;%d,", circle_position.dx, circle_position.dy);
+					send(client_sock, posit, strlen(posit), 0);
+					print_bottom("Circle x y pos is %d :  %d\n", circle_position.dx, circle_position.dy);
+				}else if (circle_position.dy < 25 && circle_position.dy > -25 && circle_position.dx < 25 && circle_position.dx > -25 && !run_main_loop) {
+					send(client_sock, "0", 1, 0);
+					
+					print_bottom("we are stopped\n");
+					run_main_loop = false;
 				}
 
-				if (cpos.dy < 25 && cpos.dy > -25 && cpos.dx < 25 && cpos.dx > -25 && stop == false) {
-					send(csock, "0", 1, 0);
-					printf("we are stopped\n");
-					stop = true;
-				}
 				gspWaitForVBlank();
 				gfxFlushBuffers();
 				gfxSwapBuffers();
@@ -187,8 +263,8 @@ int main(int argc, char** argv) {
 
 	}
 
-	csock = -1;
-	close(sock);
+	client_sock = -1;
+	close(server_sock);
 	gfxExit();
 	return 0;
 }
@@ -202,7 +278,7 @@ void failExit(const char* fmt, ...) {
 
 	va_list ap;
 
-	printf(CONSOLE_RED);
+	print(CONSOLE_RED);
 	va_start(ap, fmt);
 	vprintf(fmt, ap);
 	va_end(ap);
